3e64b666e341c6c31818a577727bd2b37d0a62cc
Yashraj Nagpal <nagpal.yashraj@gmail.com>
2022-02-27 13:50:35 -0500

    Updated experience

--- JS/YashrajNagpalDarkModeJS.js
+++ JS/YashrajNagpalDarkModeJS.js
@@ -17,3 +17,34 @@ document.querySelector('.darkMode').addEventListener('click', () =>
 	}
 
 })
+
+
+function collapse(){
+	var collapse = document.getElementsByClassName("coll")[0];
+	var collbtn = document.getElementsByClassName("collapsible")[0];
+	
+	if(collapse.style.display == "none"){
+		collapse.style.display = "block"
+		collbtn.innerHTML = "Less"
+	}else{
+		collapse.style.display = "none"
+		collbtn.innerHTML = "More"
+	}
+}
+
+
+// var i;
+
+// for (i = 0; i < coll.length; i++) {
+//   coll[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var content = this.nextElementSibling;
+//     if (content.style.display === "block") {
+//       content.style.display = "none";
+// 	//   coll.value = "More";
+//     } else {
+//       content.style.display = "block";
+// 	  coll.value = "Less";
+//     }
+//   });
+// }
